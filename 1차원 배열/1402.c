196136cc89172db5543142616561dd9f0d72435c
kimsiyeon0223 <Ksyeon0223@gmail.com>
2023-06-14 12:15:51 +0900

    0614

---
+++ 1차원 배열/1402.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    int arr[1000]={};
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        scanf("%d ", &arr[i]);
+    }
+    for(int i=n; i>=1; i--){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
