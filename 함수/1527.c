0649e058d4afdef32d05c4790ea5a4f4fa0dc645
kimsiyeon0223 <Ksyeon0223@gmail.com>
2023-06-15 17:18:53 +0900

    0615

---
+++ 함수/1527.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void f(){
+    printf("123");
+    return;
+}
+
+int main(){
+    f();
+    return 0;
+}
---
+++ 함수/1529.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void f(){
+    printf("**");
+    return;
+}
+
+int main(){
+    f();
+    return 0;
+}
