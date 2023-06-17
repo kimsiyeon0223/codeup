aaf0d54561a1bf50faa73bd5c422dd538d829a71
kimsiyeon0223 <Ksyeon0223@gmail.com>
2023-06-17 18:06:30 +0900

    0617

---
+++ 함수/재귀함수/1912.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int result=1;
+void f(int k){
+    if(k==0)
+        return;
+    f(k-1);
+    result=result*k;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    f(n);
+    printf("%d", result);
+    return 0;
+}
