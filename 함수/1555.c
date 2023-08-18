9014a9f0d28128cf53f3fa7cc84d5c329197449a
kimsiyeon0223 <Ksyeon0223@gmail.com>
2023-08-18 22:19:07 +0900

    0818

---
+++ 함수/1555.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int n;
+
+int f(int n){
+	long long int result=0;
+	for(int i=1; i<=n; i++){
+		result+=i;
+	}
+	return result;
+}
+
+int main(){
+	scanf("%d", &n);
+	printf("%lld\n", f(n));
+}
